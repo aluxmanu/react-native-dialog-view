29990743a6ba191b0dd3f8ba64079f135204b812
Manu Alex <alexmanu.work@gmail.com>
2023-07-22 01:08:36 +0300

    fix: type props

--- src/components/DialogView/DialogViewProps.ts
+++ src/components/DialogView/DialogViewProps.ts
@@ -1,13 +1,25 @@
 import { ReactNode } from 'react';
 import { ViewStyle } from 'react-native';
-import { ComplexAnimationBuilder } from 'react-native-reanimated';
+import {
+  BaseAnimationBuilder,
+  EntryExitAnimationFunction,
+  Keyframe,
+} from 'react-native-reanimated';
 
 export type DialogViewProps = {
   children: ReactNode;
   visible: boolean;
   animationTime?: number;
-  animationIn?: ComplexAnimationBuilder;
-  animationOut?: ComplexAnimationBuilder;
+  animationIn?:
+    | BaseAnimationBuilder
+    | typeof BaseAnimationBuilder
+    | EntryExitAnimationFunction
+    | Keyframe;
+  animationOut?:
+    | BaseAnimationBuilder
+    | typeof BaseAnimationBuilder
+    | EntryExitAnimationFunction
+    | Keyframe;
   backdropColor?: string;
   overlayStyle?: ViewStyle;
   onPressBackdrop?: () => void;
